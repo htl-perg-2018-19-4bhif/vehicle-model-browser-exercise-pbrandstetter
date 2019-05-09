a2f9543db630f56a2a6496eb779ecad8407594e1
Philipp Brandstetter <philipp.brandstetter57@gmail.com>
2019-05-09 08:16:34 +0200

    Style changes

--- vehicle-browser/src/app/app.component.ts
+++ vehicle-browser/src/app/app.component.ts
@@ -4,14 +4,9 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <h1>Vehicle Browser</h1>
-    <ul>
-      <li>
-        <a routerLink='models'>Models</a>
-      </li>
-      <li>
-        <a routerLink='about'>About</a>
-      </li>
-    </ul>
+    <button mat-fab routerLink='about'> About </button>&nbsp;
+    <button mat-fab routerLink='models'> Models </button>
+    <br><br>
     <router-outlet></router-outlet>
   `,
   styles: [],
